527a1acb2bdcd085ab13e87828078df980e7efb4
Paul <canfield010@gannon.edu>
2021-04-06 07:22:20 -0400

    Gonna solve the puzzlegit add .! EPIC!

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static enum operation {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        MODULO,
+        SIN,
+        COS,
+        TAN,
+        EXP
+    }
+
+}
